a56f4949251b8d91da6a85aa4ceecfc41a3f3e94
wj831229 <grass_tree@naver.com>
2026-04-19 15:25:09 +0900

    Auto save: 2026-04-19 15:25:09

--- Assets/Scripts/SurvivorInfoPanel.cs
+++ Assets/Scripts/SurvivorInfoPanel.cs
@@ -134,7 +134,7 @@ public class SurvivorInfoPanel : MonoBehaviour
                          : Color.red;
 
         if (currentSurvivor.inventory.Count == 0)
-            inventoryText.text = "(empty)";
+            inventoryText.text = "(비어있음)";
         else
         {
             var counts = new System.Collections.Generic.Dictionary<string, int>();
