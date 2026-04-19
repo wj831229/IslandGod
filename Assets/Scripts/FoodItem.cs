5aed544fd62dcacbb62b5dc7a86f6f097ec9b2b3
wj831229 <grass_tree@naver.com>
2026-04-19 15:45:26 +0900

    Auto save: 2026-04-19 15:45:26

--- Assets/Scripts/FoodItem.cs
+++ Assets/Scripts/FoodItem.cs
@@ -4,6 +4,8 @@ public class FoodItem : MonoBehaviour
 {
     public FoodPoint foodPoint;
     public float hungerRestore = 40f;
+    public int gatherAmount = 5;
+    public string itemName = "코코넛";
 
     void Start()
     {
